6796c5c77f220f7dac8b1e561f298e0135d46d48
Mackenzie <mackenzie.techdocs@gmail.com>
2025-08-24 19:48:19 -0400

    feat(WTA-8): fixing the sidebar.

--- site/sidebars.js
+++ site/sidebars.js
@@ -1,57 +1,48 @@
 // @ts-check
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
 /**
  * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
-
- @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ * - create an ordered group of docs
+ * - render a sidebar for each doc of that group
+ * - provide next/previous navigation
+ *
+ * The sidebars can be generated from the filesystem, or explicitly defined here.
+ *
+ * Create as many sidebars as you want.
+ *
+ * @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
  */
 const sidebars = {
-  // By default, Docusaurus generates a sidebar from the docs folder structure
   portfolioSidebar: [
     'intro',
     {
       type: 'category',
       label: 'Portfolio',
       items: [
-  'documentation-tooling/index',
-  'documentation-tooling/astronomer-doc-audit',
-
-
-        'tutorial-basics/create-a-document',
-        'tutorial-basics/create-a-blog-post',
-        'tutorial-basics/deploy-your-site',
+        'documentation-tooling/index',
+        'documentation-tooling/astronomer-doc-audit',
       ],
     },
     {
       type: 'category',
       label: 'Writing Samples',
       items: [
+        'tutorial-basics/create-a-document',
+        'tutorial-basics/create-a-blog-post',
+        'tutorial-basics/deploy-your-site',
         'tutorial-basics/markdown-features',
+        'tutorial-basics/congratulations',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Extras',
+      items: [
+        'tutorial-extras/manage-docs-versions',
+        'tutorial-extras/translate-your-site',
       ],
     },
   ],
-
-  // But you can create a sidebar manually
-  /*
-  tutorialSidebar: [
-    'intro',
-    'hello',
-    {
-      type: 'category',
-      label: 'Tutorial',
-      items: ['tutorial-basics/create-a-document'],
-    },
-  ],
-   */
 };
 
 export default sidebars;
